a5e8dcc9b1d8ac2711a7b44422d488fabae06ed4
theog48 <theo.grasset.dwwm48@gmail.com>
2024-12-11 14:26:09 +0100

    Update

--- doc-html-css-js/script.js
+++ doc-html-css-js/script.js
@@ -36,3 +36,25 @@ document.getElementById('formulaire').addEventListener('submit', function(event)
     alert("Formulaire validé !");
     event.target.submit();
 });
+// Récupère l'élément de la modale et l'image
+const modal = document.getElementById('modal');
+const modalImage = document.getElementById('modal-image');
+const closeBtn = document.getElementById('close-btn');
+
+// Sélectionne toutes les images de la galerie
+const images = document.querySelectorAll('.image_item img');
+
+images.forEach(image => {
+image.addEventListener('click', function() {
+    modal.style.display = 'block';
+    modalImage.src = image.src;
+});
+});
+closeBtn.addEventListener('click', function() {
+modal.style.display = 'none';
+});
+window.addEventListener('click', function(event) {
+if (event.target === modal) {
+    modal.style.display = 'none';
+}
+});
