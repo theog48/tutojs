c113f41d07732a82339b9477d00bbb813723ed74
theog48 <theo.grasset.dwwm48@gmail.com>
2025-01-07 14:16:40 +0100

    update

---
+++ doc-html-css-js/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:C:/Users/theo/Desktop/Cours/tuto-js/doc-html-css-js/**/*/.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
